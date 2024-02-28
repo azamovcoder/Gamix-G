1b4cd994020e06c3f68964f06a04ac7b71625cb1
azamovcoder <azamovcoder@gmail.com>
2024-02-28 19:58:30 +0300

    all

--- js/carusel.js
+++ js/carusel.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         loop: true,
         margin: 10,
+        nav:true,
         responsive: {
             0: {
                 items: 1,
